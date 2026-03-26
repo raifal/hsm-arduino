9ed98604e380edbdfd89e17c3aefe9ace717d016
Rainer Faller <raifal@users.noreply.github.com>
2026-03-26 19:42:58 +0100

    v1.6

--- hsm/hsm.ino
+++ hsm/hsm.ino
@@ -5,9 +5,10 @@
  *
  * for Arduino Uno with DS18B20 temperature sensors
  *
- * Version 1.5
+ * Version 1.6
  * Author: Rainer Faller (www.rainer-faller.de)
  * Created: 17/MAY/2014
+ * Last Update: 26/MAR/2026
  *
  * Supports Two 1-Wire Buses for temperature measurement
  * Supports water leak level alarm
@@ -16,7 +17,7 @@
  *
  * Update V 1.4: Includes a water leak detection
  * Update V 1.5: Includes a second water leak detection 
- *
+ * Update V 1.6: Use local PI, single transmits instead of bulk.
  */
 
 // Includes
@@ -27,23 +28,22 @@
 #include <Ethernet.h>
 
 // MetaData
-const char VERSION[5] = "1.5";
-const char CREATED[12] = "17/MAY/2014";
+const char VERSION[5] = "1.6";
+const char CREATED[12] = "26/MAR/2026";
 
 // ************************
 // Individual Parameters
 // ************************
-const char      CLIENT_CODE[] = "r$f%667"; 
       byte      MAC_OF_ARDUINO_BOARD[] = { 0x90, 0xA2, 0xDA, 0x0E, 0xC8, 0x42 }; 
 const IPAddress ARDUINO_CLIENT_IP (192,168,0,177);
 
-const char      TARGET_SERVER[] = "server";
-const int       TARGET_PORT = <port>;
-const char      TARGET_ENDPOINT[] = "/";
+const char      TARGET_SERVER[] = "192.168.0.46";
+const int       TARGET_PORT = 8001;
+const char      TARGET_ENDPOINT[] = "/api/measurements/batch";
 
 // TODO add base64 encoded string from Keepass "HZS php (called by Arduino)"
 // generate with: bash: echo -n "$username:$password" | base64
-const char      BASE64_AUTH_STRING[] = "<base64 encoded string>";
+const char      BASE64_AUTH_STRING[] = "aHNtOnNlQ3JldGk0aHNtX2FwaQ==";
 
 // ************************
 // Board Setup (Pin#)
@@ -51,12 +51,12 @@ const char      BASE64_AUTH_STRING[] = "<base64 encoded string>";
    
 #define ONE_WIRE_BUS_1   2
 #define ONE_WIRE_BUS_2   3
-#define PIN_LOWER_WATER_LEAK   A0
-#define PIN_UPPER_WATER_LEAK   A1
+// #define PIN_LOWER_WATER_LEAK   A0
+// #define PIN_UPPER_WATER_LEAK   A1
 #define PIN_LED_LOOP_TOGGLE             5
-#define PIN_LED_SWITCH_SEND_IMMEDIATLY  6
-#define PIN_SWITCH_SEND_IMMEDIATLY      7
-#define PIN_LED_COLLECTION_BLOCK_TOGGLE 8
+// #define PIN_LED_SWITCH_SEND_IMMEDIATLY  6
+// #define PIN_SWITCH_SEND_IMMEDIATLY      7
+// #define PIN_LED_COLLECTION_BLOCK_TOGGLE 8
 
 // sum of both 1-WireBuses
 #define MAX_SENSORS      12 
@@ -74,11 +74,9 @@ typedef struct
 // ************************
 const int       ONE_SECOND = 1000;
 const byte      NO_SENSOR_CONNECTED[8] = {0,0,0,0,0,0,0,0};
-// one loop is approx. 1 second  // recommend: 120
-const int       NUMBER_OF_LOOPS_UNTIL_NEXT_MEASUREMENT = 120; 
-// how many measurements are stored. recommended: 5 (seems to be max)
-const int       SENDING_BLOCK_SIZE = 5; 
-const int       CRITICAL_WATER_LEAK_VALUE = 800;
+// how often to measure and send the temperature data  // recommend: 120
+const int       NUMBER_OF_LOOPS_UNTIL_NEXT_MEASUREMENT = 5; 
+//const int       CRITICAL_WATER_LEAK_VALUE = 800;
 
 
 // ************************
@@ -94,21 +92,13 @@ DallasTemperature temperatureSensors2( &oneWireBus2 );
 // ************************
 SENSOR_ADDRESS connected_sensor_ids[MAX_SENSORS];
 // stores temperature value * 100
-int            measurement_values[SENDING_BLOCK_SIZE][MAX_SENSORS]; 
+int            measurement_values[MAX_SENSORS]; 
 int            number_of_connected_sensors;
 int            loop_counter_until_next_measurement;
-int            current_measurement_index;
 char           tempAddr[40];
-boolean        sendNow;
-boolean        loopToggle;
-boolean        sendingToggle;
 EthernetClient client;
 // state of the connection last time through the main loop
 boolean        lastConnected = false;
-int            lower_water_leak_voltage = 1023; // MAX Voltage
-int            upper_water_leak_voltage = 1023; // MAX Voltage
-boolean        exceptional_lower_water_leak_sent = false;
-boolean        exceptional_upper_water_leak_sent = false;
 
 
 void setup() 
@@ -119,9 +109,9 @@ void setup()
   Serial.begin(9600);
   printInfoHeader();
   
-  pinMode(PIN_SWITCH_SEND_IMMEDIATLY,INPUT);
-  pinMode(PIN_LED_COLLECTION_BLOCK_TOGGLE,OUTPUT);
-  pinMode(PIN_LED_SWITCH_SEND_IMMEDIATLY, OUTPUT);
+  //pinMode(PIN_SWITCH_SEND_IMMEDIATLY,INPUT);
+  //pinMode(PIN_LED_COLLECTION_BLOCK_TOGGLE,OUTPUT);
+  //pinMode(PIN_LED_SWITCH_SEND_IMMEDIATLY, OUTPUT);
   pinMode(PIN_LED_LOOP_TOGGLE, OUTPUT);
   
   delay(2*ONE_SECOND);
@@ -138,10 +128,6 @@ void setup()
   printConnectedSensors();
   
   loop_counter_until_next_measurement = NUMBER_OF_LOOPS_UNTIL_NEXT_MEASUREMENT;
-  current_measurement_index = 0;
-  sendNow = false;
-  loopToggle = true;
-  sendingToggle = true;
   
   Serial.println();
   Serial.println("Setup done, starting temperature measurement and transmitting");
@@ -170,89 +156,23 @@ void loop()
   if ( loop_counter_until_next_measurement >= NUMBER_OF_LOOPS_UNTIL_NEXT_MEASUREMENT )
   {
      measureTemperature();
-     current_measurement_index++;
+     sendToServer();
      loop_counter_until_next_measurement = 0;
-  }
-  
-  if ( digitalRead(PIN_SWITCH_SEND_IMMEDIATLY) == HIGH)
-  {
-    digitalWrite(PIN_LED_SWITCH_SEND_IMMEDIATLY,HIGH);
-    sendNow = true;
-    
-  }
-  else
-  {
-    digitalWrite(PIN_LED_SWITCH_SEND_IMMEDIATLY,LOW);
-  } 
-  
-  lower_water_leak_voltage = analogRead( PIN_LOWER_WATER_LEAK );
-  upper_water_leak_voltage = analogRead( PIN_UPPER_WATER_LEAK );
-  //Serial.println(water_leak_voltage);
-  if ( lower_water_leak_voltage < CRITICAL_WATER_LEAK_VALUE )
-  {
-    if ( exceptional_lower_water_leak_sent == false )
-    {
-       sendNow = true;
-       Serial.println("Lower water leak level critical, sending exceptional message");
-       exceptional_lower_water_leak_sent = true; // only sent once 
-    }
-  }
-  else
-  {
-    exceptional_lower_water_leak_sent = false; // water leak back to normal
-  }
-  
-  if ( upper_water_leak_voltage < CRITICAL_WATER_LEAK_VALUE )
-  {
-    if ( exceptional_upper_water_leak_sent == false )
-    {
-       sendNow = true;
-       Serial.println("Upper water leak level critical, sending exceptional message");
-       exceptional_upper_water_leak_sent = true; // only sent once 
-    }
-  }
-  else
-  {
-    exceptional_upper_water_leak_sent = false; // water leak back to normal
   }  
-  
-  // send to internet server
-  if ( current_measurement_index >= SENDING_BLOCK_SIZE || sendNow == true )
-  {
-    sendToServer();
-    current_measurement_index = 0;
-    sendNow = false;
-    
-      // sending indicator
-    if ( sendingToggle == true )
-    {
-      digitalWrite(PIN_LED_COLLECTION_BLOCK_TOGGLE,HIGH);
-      sendingToggle = false;
-    }
-    else
-    {
-      digitalWrite(PIN_LED_COLLECTION_BLOCK_TOGGLE,LOW);
-      sendingToggle = true;
-    } 
-  }
-
 
   // store the state of the connection for next time through
   // the loop:
   lastConnected = client.connected();
-  
-  // active loop indicator
-  if ( loopToggle == true )
+
+ // Serial.println("Blink");
+  if ( digitalRead(PIN_LED_LOOP_TOGGLE) == HIGH ) 
   {
-    digitalWrite(PIN_LED_LOOP_TOGGLE,HIGH);
-    loopToggle = false;
+    digitalWrite(PIN_LED_LOOP_TOGGLE, LOW);  
   }
   else
   {
-    digitalWrite(PIN_LED_LOOP_TOGGLE,LOW);
-    loopToggle = true;
-  }   
-
+    digitalWrite(PIN_LED_LOOP_TOGGLE, HIGH);
+  }
   delay(ONE_SECOND);
   
   loop_counter_until_next_measurement++;
@@ -393,97 +313,34 @@ void measureTemperature()
     Serial.print(temp_of_sensor);
     Serial.println(" Grad Celcius");
     
-    measurement_values[current_measurement_index][s] = temp_of_sensor * 100;    
+    measurement_values[s] = temp_of_sensor * 100;    
   } 
 }
 
 void sendToServer()
 {
-  /*
+  
   Serial.println();
   Serial.println("Debug is on, not sending to server");
-  for ( int i=0; i<current_measurement_index; i++ )
-  {
-    int ts = -( current_measurement_index - i -1 ) * NUMBER_OF_LOOPS_UNTIL_NEXT_MEASUREMENT;
-    for ( int s = 0; s<number_of_connected_sensors; s++)
-    {
-      Serial.print("op=m:");
-      Serial.print("ts=");
-      Serial.print( ts );
-      Serial.print(":addr=");
-      Serial.print(sensorAddressToString(tempAddr, connected_sensor_ids[s].address));
-      Serial.print(":value=");
-      Serial.print(measurement_values[i][s]);
-      Serial.println();
-    }
-  }
+
+  Serial.print("{ \"measurements\": [ ");
   Serial.println();
-  */
   
-  Serial.println("Connecting to server...");
-  
-  delay(2 * ONE_SECOND);
-  
-  if (client.connect(TARGET_SERVER, TARGET_PORT)) 
+  for ( int s = 0; s<number_of_connected_sensors; s++)
   {
-
-    Serial.print("Sending data to server ");
-    Serial.print(TARGET_SERVER);
-    Serial.print(":");
-    Serial.print(TARGET_PORT);
-    Serial.println("... ");
-    
-    // send the HTTP GET request:
-    client.print("GET ");
-    client.print(TARGET_ENDPOINT);
-    client.println(" HTTP/1.1");
-    client.print("Host: ");
-    client.println(TARGET_SERVER);
-    client.print("Authorization: Basic ");
-    client.println(BASE64_AUTH_STRING);   
-    client.println("User-Agent: arduino-ethernet");
-      
-    // send client code first
-    client.print("HSM-C:");
-    client.print("client=");
-    client.print(CLIENT_CODE);
-    client.println();
-    
-    // send water leaking voltage value (5V=1023)
-    client.print("HSM-W:");
-    client.print("lower_voltage=");
-    client.print(lower_water_leak_voltage);
-    client.print(":");
-    client.print("upper_voltage=");
-    client.print(upper_water_leak_voltage);    
-    client.println();    
-    
-    for ( int i=0; i < current_measurement_index; i++)
-    {     
-      int ts = - ( current_measurement_index - i -1 ) * NUMBER_OF_LOOPS_UNTIL_NEXT_MEASUREMENT;
-      for ( int s = 0; s < number_of_connected_sensors; s++)
-      {
-        client.print("HSM-M");
-        client.print(i);
-        client.print("x");
-        client.print(s);
-        client.print(":");
-        client.print("ts=");
-        client.print( ts );
-        client.print(":addr=");
-        client.print(sensorAddressToString(tempAddr, connected_sensor_ids[s].address));
-        client.print(":value=");
-        client.print(measurement_values[i][s]);
-        client.println();
-      }
-    }
-      
-    client.println("Connection: close");
-    client.println();
-  } 
-  
+    if ( s != 0 ) { Serial.print(","); }
+    Serial.print("{ \"sensorAddress\": \"");
+    Serial.print(sensorAddressToString(tempAddr, connected_sensor_ids[s].address));
+    Serial.print("\", \"temperature\": ");
+    Serial.print(measurement_values[s]);
+    Serial.print(", \"timestamp\": \"");
+    Serial.print("2026-03-08T18:32:07.159Z");
+    Serial.print("\" }");
+    Serial.println();
+  }
+  Serial.print("] }");   
+  Serial.println();
 }
-
 
 void printInfoHeader()
 {
