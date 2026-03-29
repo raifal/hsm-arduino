08ce2d009140f4e72c33bd2c7e8ef460c4fea026
Rainer Faller <raifal@users.noreply.github.com>
2026-03-29 20:12:54 +0200

    13 sensors now

--- hsm/hsm.ino
+++ hsm/hsm.ino
@@ -43,18 +43,18 @@ const char      TARGET_ENDPOINT[] = "/api/measurements/batch";
 
 // TODO add base64 encoded string from Keepass "HZS php (called by Arduino)"
 // generate with: bash: echo -n "$username:$password" | base64
-const char      BASE64_AUTH_STRING[] = "<HERE>";
+const char      BASE64_AUTH_STRING[] = "here base64";
 
 // ************************
 // Board Setup (Pin#)
 // ************************
    
-#define ONE_WIRE_BUS_1   2
-#define ONE_WIRE_BUS_2   3
-#define PIN_LED_LOOP_TOGGLE             5
+#define ONE_WIRE_BUS_1      2
+#define ONE_WIRE_BUS_2      3
+#define PIN_LED_LOOP_TOGGLE 5
 
 // sum of both 1-WireBuses
-#define MAX_SENSORS      12 
+#define MAX_SENSORS         14 
 
 typedef struct 
 {
@@ -93,6 +93,21 @@ char           tempAddr[40];
 EthernetClient client;
 // state of the connection last time through the main loop
 boolean        lastConnected = false;
+
+
+
+extern unsigned int __heap_start;
+extern void *__brkval;
+int freeMemory() {
+  int free_memory;
+  if ((int)__brkval == 0) {
+    free_memory = ((int)&free_memory) - ((int)&__heap_start);
+  } else {
+    free_memory = ((int)&free_memory) - ((int)__brkval);
+  }
+  return free_memory;
+}
+
 
 
 void setup() 
@@ -146,6 +161,8 @@ void loop()
   // measure temperature  
   if ( loop_counter_until_next_measurement >= NUMBER_OF_LOOPS_UNTIL_NEXT_MEASUREMENT )
   {
+     Serial.print("Freier RAM: ");
+     Serial.println(freeMemory());
      measureTemperature();
      sendToServer();
      loop_counter_until_next_measurement = 0;
@@ -321,7 +338,8 @@ void sendToServer()
   client.print("Authorization: Basic ");
   client.print(BASE64_AUTH_STRING);
   client.print("\r\n");
-  client.print("Content-Type: application/json\r\n");   
+  client.print("Content-Type: application/json\r\n");
+  //client.print("Connection: close\r\n");   
   client.print("Content-Length: ");
   if ( number_of_connected_sensors == 0 )
   {
@@ -329,11 +347,11 @@ void sendToServer()
   }
   else if ( number_of_connected_sensors == 1)
   {
-    client.print(17+79+2);
+    client.print(17+78+2);
   }
   else
   {
-    client.print(17+79+ ((number_of_connected_sensors-1)*80) +2);
+    client.print(17+78+ ((number_of_connected_sensors-1)*79) +2);
   }
   client.print("\r\n");
   
